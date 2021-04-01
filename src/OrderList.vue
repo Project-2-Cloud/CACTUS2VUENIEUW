26c5f5689673917b61258b2b4906c18b184b354f
KobeLiesenborgs <kobe.liesenborgs1998@gmail.com>
2021-04-02 00:19:23 +0200

    fix shopping cart

--- src/OrderList.vue
+++ src/OrderList.vue
@@ -3,42 +3,6 @@
     <a @click="onLogoutClicked" class="dropdown-item"
     >My orders </a
     >
-    <div id="orderList" class="modal fade">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">
-              <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-              Shopping cart
-            </h5>
-            <button class="close" data-dismiss="modal">
-              &times;
-            </button>
-          </div>
-          <div class="modal-body">
-            <table class="table">
-              <tbody>
-              <tr v-for="(item,idx) in inCart" :key="idx">
-                <td>{{ item.title }}</td>
-                <td>${{ item.price }}</td>
-                <td>
-                  <button class="btn btn-sm btn-danger" @click="removeFromCart(item)">&times;</button>
-                </td>
-              </tr>
-              <tr>
-                <th></th>
-                <th>${{ total }}</th>
-                <th></th>
-              </tr>
-              </tbody>
-            </table>
-          </div>
-          <div class="modal-footer">
-            <button class="btn btn-secondary" data-dismiss="modal">Back to shop</button>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 <script>
